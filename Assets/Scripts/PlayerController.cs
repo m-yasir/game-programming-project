cb6cbd25d23247a3f25575dc2b4caa2cfb0baf60
Yasir Nadeem <m-yasir.dev@protonmail.com>
2020-06-18 11:43:36 +0500

    Added stickman figure and moved the limbs to appropriate positions. Attached 2d colliders, rigidbodies and hinge joints to stickman. Add pull script to pull character on left click. Use rigidbody forces on bones to make character stand.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 mouse = (Vector2)Input.mousePosition;
+
+        //gameObject.GetComponent<Camera>();
     }
 }
---
+++ Assets/Scripts/Stand.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stand : MonoBehaviour
+{
+    public _Muscle[] muscles;
+
+    private void FixedUpdate()
+    {
+        foreach (var muscle in muscles)
+        {
+            muscle.ActivateMuscle();
+        }
+    }
+}
+
+
+[System.Serializable]
+public class MUSCLE
+{
+    public Rigidbody2D bone;
+    public float restRotation;
+    public float force;
+
+    public void ActivateMuscle()
+    {
+        bone.MoveRotation(Mathf.LerpAngle(bone.rotation, restRotation, force * Time.deltaTime));
+    }
+}
